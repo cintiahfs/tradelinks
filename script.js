212c0f156900a8cd067f54a796d67b0cae74b02e
cintia_segovia <cintia.segovia@original.io>
2023-07-30 19:28:41 -0300

    feat: Add new homepage to choose profile

--- script.js
+++ script.js
@@ -2,15 +2,34 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
-  // pegar a tag img
-  const img = document.querySelector("#profile img")
+  const light = html.classList.contains("light")
+  const cards = document.getElementById("cards")
 
-  // substituir a imagem
-  if (html.classList.contains("light")) {
-    // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
-  } else {
-    // set tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+  if (cards) {
+    const imgprofile1 = cards.querySelector("img.profile_1")
+    const imgprofile2 = cards.querySelector("img.profile_2")
+    if (light) {
+      imgprofile1.setAttribute("src", "./assets/avatar-light.png")
+      imgprofile1.setAttribute(
+        "alt",
+        "Foto do Mayk Brito usando óculos escuros, camisa preta e jaqueta, em um fundo azulado."
+      )
+      imgprofile2.setAttribute("src", "./assets/avatar-light-2.png")
+      imgprofile2.setAttribute(
+        "alt",
+        "Foto da Cíntia Segóvia de óculos escuros e de cabelo solto, com a Casa Branca ao fundo."
+      )
+    } else {
+      imgprofile1.setAttribute("src", "./assets/avatar.png")
+      imgprofile1.setAttribute(
+        "alt",
+        "Foto do Mayk Brito usando óculos e camisa preta, em um fundo amarelo."
+      )
+      imgprofile2.setAttribute("src", "./assets/avatar-2.png")
+      imgprofile2.setAttribute(
+        "alt",
+        "Foto da Cíntia Segóvia sorrindo e de cabelo solto, em um fundo branco."
+      )
+    }
   }
 }
